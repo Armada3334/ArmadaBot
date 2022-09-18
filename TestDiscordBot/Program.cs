1288950732e5863e25c492c1b508facdc3854ab5
Armada <73616858+Armada3334@users.noreply.github.com>
2022-09-18 17:19:14 -0400

    Delete unnecessary files, added comments.

--- TestDiscordBot/Program.cs
+++ TestDiscordBot/Program.cs
@@ -29,20 +29,21 @@ namespace ArmadaBot
                 Intents = DiscordIntents.All
             });
 
+            // Create a logger to output into the console
             new DiscordConfiguration()
             {
                 MinimumLogLevel = LogLevel.Debug,
                 LogTimestampFormat = "MMM dd yyyy - hh:mm:ss tt"
             };
 
-            // Create a new command handler
+            // Create a new command handler with the prefix of "!"
             var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
             {
                     StringPrefixes = new[] { "!" }
             });
             commands.RegisterCommands(Assembly.GetExecutingAssembly());
 
-
+            // Connect to the discord bot and wait to prevent the console window from closing prematurely
             await discord.ConnectAsync();
             await Task.Delay(-1);
         }
